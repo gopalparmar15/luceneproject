bbee483bb9558abe57dae6c8d4ffea14381ce1ec
gopalparmar15 <gopalp@prominentpixel.com>
2020-10-27 19:43:24 +0530

    four commit

--- luceneproject/src/main/java/com/prominentpixel/CsvFileIndexing.java
+++ luceneproject/src/main/java/com/prominentpixel/CsvFileIndexing.java
@@ -7,6 +7,8 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -34,28 +36,33 @@ public class CsvFileIndexing {
         IndexWriter writer=new IndexWriter(fsDirectory,config);
         System.out.print("Properties Csv File Path Key=");
         String csvFilePath=scanner.next();
-        List<String> fileLines= Files.readAllLines(Paths.get(properties.getProperty(csvFilePath)));
-
-
-        SimpleDateFormat dateFormat=new SimpleDateFormat("dd-MMM-yy");
-        //Take Header String here it will always be at the 0th Index.
-        String headerLine = fileLines.get(0);
-        String[] headers = headerLine.split(",");
-        for (String line:fileLines)
+        File file=new File(properties.getProperty(csvFilePath));
+        String[] list=file.list();
+        for (String fileName:list)
         {
-            String[] data=line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)",-1);
-            try {
-                Document document=new Document();
-                int dataIndex = 0;
-                for(String header : headers){
-                    document.add(new StringField(header, data[dataIndex++], Field.Store.YES));
+            List<String> fileLines= Files.readAllLines(Paths.get(file+"/"+fileName));
+            SimpleDateFormat dateFormat=new SimpleDateFormat("dd-MMM-yy");
+            //Take Header String here it will always be at the 0th Index.
+            String headerLine = fileLines.get(0);
+            String[] headers = headerLine.split(",");
+            for (String line:fileLines)
+            {
+                String[] data=line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)",-1);
+                try {
+                    Document document=new Document();
+                    int dataIndex = 0;
+                    for(String header : headers){
+                       document.add(new StringField(header, data[dataIndex++], Field.Store.YES));
+                        /*System.out.print(header+"="+data[dataIndex++]+"=")*/;
+                    }
+                    writer.addDocument(document);
                 }
-                writer.addDocument(document);
-            }
-            catch (IOException e) {
-                e.printStackTrace();
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
+
         scanner.close();
         writer.commit();
         writer.close();
